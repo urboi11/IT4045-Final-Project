19252a1032bb4d1a0eb0a199c5ada62cda2396cc
Jason Welsh <132491235+urboi11@users.noreply.github.com>
2025-09-23 07:54:49 -0400

    Pushing intial files.

---
+++ src/test/java/com/it4045/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it4045.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
