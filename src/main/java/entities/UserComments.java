9876d9cb54e5b4e71c82b1bd19d680efa4453e72
Sage Bushstone <sage.bushstone@gmail.com>
2025-10-03 07:51:31 -0400

    preliminary UserComments lombok

--- src/main/java/entities/UserComments.java
+++ src/main/java/entities/UserComments.java
@@ -1,4 +1,24 @@
 package entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="usercomments")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class UserComments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userCommentId;
+
+    private int userId;
+    private int courseId;
+    private String comment;
 }
