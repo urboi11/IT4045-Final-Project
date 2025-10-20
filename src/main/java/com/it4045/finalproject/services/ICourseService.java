a53e48b97383f1bdf4b8f26c55fb492bc4352186
goudyeaUC <156120163+goudyeaUC@users.noreply.github.com>
2025-10-20 14:30:57 -0400

    Skeleton for Course service

--- src/main/java/com/it4045/finalproject/services/ICourseService.java
+++ src/main/java/com/it4045/finalproject/services/ICourseService.java
@@ -1,4 +1,13 @@
 package com.it4045.finalproject.services;
 
 public interface ICourseService {
+   // public void CreateUser(String FirstName, String LastName, String Email, String Password, Boolean IsAdmin);
+
+     //public void GetUserName(int userID);
+
+    //public void getUsers();
+
+
+
+
 }
