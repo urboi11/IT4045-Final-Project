44baef8c795390eeb79ea2625e426146feac9b22
Jason Welsh <jason.michael.welsh@outlook.com>
2025-10-22 15:26:58 -0400

    Implementation of UserService and IUserService Classes

--- src/main/java/com/it4045/finalproject/services/UserService.java
+++ src/main/java/com/it4045/finalproject/services/UserService.java
@@ -1,5 +1,45 @@
 package com.it4045.finalproject.services;
 
-public class  UserService implements ICourseService{
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.it4045.finalproject.entities.UserComments;
+import com.it4045.finalproject.entities.User;
+import com.it4045.finalproject.repository.UserCommentRepository;
+import com.it4045.finalproject.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class  UserService implements IUserService{
+
+
+    private UserRepository userRepository;
+
+    private UserCommentRepository userCommentRepository;
+    
+    @Override
+    public User createUser(User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public String getUserName(int userId) {
+        return userRepository.findById(userId).get().getUserEmail();
+
+
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<UserComments> getCommentsForUser(User user) {
+        return userCommentRepository.findByUser(user.getUserId());
+    }
 
 }
