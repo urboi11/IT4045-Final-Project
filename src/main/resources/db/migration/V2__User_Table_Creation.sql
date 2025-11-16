8972673e5964810f1a9f32beb35eea1e9f91da61
Jason Welsh <132491235+urboi11@users.noreply.github.com>
2025-11-16 13:12:00 -0500

    Adding revisions to Flyway Migrations

--- src/main/resources/db/migration/V2__User_Table_Creation.sql
+++ src/main/resources/db/migration/V2__User_Table_Creation.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users
 (
-    userid         INT          NOT NULL,
-    firstname VARCHAR(255) NULL,
-    lastname  VARCHAR(255) NULL,
-    email      VARCHAR(255) NULL,
-    password       VARCHAR(255) NULL,
-    `role`          VARCHAR(255)      NOT NULL,
-    CONSTRAINT pk_users PRIMARY KEY (userid)
+    user_id         INT          NOT NULL,
+    user_first_name VARCHAR(255) NULL,
+    user_last_name  VARCHAR(255) NULL,
+    user_email      VARCHAR(255) NULL,
+    user_pass       VARCHAR(255) NULL,
+    is_admin        BIT(1)       NOT NULL,
+    CONSTRAINT pk_users PRIMARY KEY (user_id)
 );
