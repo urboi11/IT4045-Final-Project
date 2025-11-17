9c875ea43a9619617d45a179b96226a78de7ba09
Jason Welsh <132491235+urboi11@users.noreply.github.com>
2025-11-17 07:00:37 -0500

    Adding back User.java

--- src/main/java/com/it4045/finalproject/entities/User.java
+++ src/main/java/com/it4045/finalproject/entities/User.java
@@ -2,7 +2,6 @@ package com.it4045.finalproject.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
