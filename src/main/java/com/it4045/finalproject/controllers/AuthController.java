014ebbcf481f7d3a2fa6f1e7743ea59b279f47dc
goudyeaUC <156120163+goudyeaUC@users.noreply.github.com>
2025-11-30 14:52:10 -0500

    JavaDoc documentation

--- src/main/java/com/it4045/finalproject/controllers/AuthController.java
+++ src/main/java/com/it4045/finalproject/controllers/AuthController.java
@@ -16,7 +16,16 @@ import com.it4045.finalproject.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
-
+/**
+ * A controller for handling user authentication and creation. The security provided by this class is used to ensure
+ * valid results in other methods.
+ *
+ * @see UserService
+ * @see LoginRequest
+ * @see SignUpRequest
+ * @author Enterprise App Development Final Project Group
+ * JavaDoc by Ethan Goudy
+ */
 
 @Controller
 @AllArgsConstructor
@@ -25,12 +34,28 @@ public class AuthController {
 
     private final UserService userService;
 
+    /**
+     * Adds a LoginRequest object to the model and returns a redirect.
+     * @param model The target model to attach the LoginRequest to.
+     * @return A redirect to /auth/login
+     * @author Enterprise App Development Final Project Group
+     */
     @GetMapping("/login")
     public String login(Model model){
         model.addAttribute("loginRequest", new LoginRequest());
         return "/auth/login";    
     }
 
+    /**
+     * Processes a LoginRequest after submission, invoking the Login service method to validate the credentials provided.
+     * If credentials are valid, writes user information to the session and redirects. Catches exceptions and redirects with error messages when provided
+     * invalid credentials.
+     * @param loginRequest the LoginRequest DTO containing the submitted credentials.
+     * @param session the session to write information to.
+     * @param redirectAttributes Used to attach error messages to redirect.
+     * @param model the target model.
+     * @return a redirect to /courses if a user successfully logs in, otherwise returns to /auth/login with an error message.
+     */
     @PostMapping("/login")
     public String login(@ModelAttribute("loginRequest") LoginRequest loginRequest, HttpServletRequest session, RedirectAttributes redirectAttributes, Model model) {
         try{
@@ -44,14 +69,33 @@ public class AuthController {
         }
 
     }
-    
 
+    /**
+     * Used to connect a SignUpRequest to the relevant form on the signup page.
+     * @param model The target model for attaching the SignUpRequest DTO
+     * @return A redirect to the signup page
+     * @see SignUpRequest
+     * @author Enterprise App Development Final Project Group
+     */
     @GetMapping("/signup")
     public String signUp(Model model){
         model.addAttribute("signUpRequest", new SignUpRequest());
         return "/auth/signup";
     }
 
+    /**
+     * Processes a SignUpRequest DTO submitted from the signup page, using the SignUp service method
+     * to validate and create a new user account. Catches exceptions due to duplicated user credentials.
+     * @param request the DTO containing the proposed user informaton
+     * @param model The model the DTO is attached to
+     * @param redirectAttributes Usecd to attach status messages to the redirect
+     * @throws AccountExistsException when a proposed user's information is checked
+     * against existing records and found to be a duplicate of an existing user
+     * @return a redirect to the login page with a status message attached via flash attribute
+     * @see SignUpRequest
+     * @see UserService#signUp(SignUpRequest)
+     * @author Enterprise App Development Final Project Group
+     */
     @PostMapping("/signup")
     public String signUp(@ModelAttribute("signUpRequest") SignUpRequest request, Model model, RedirectAttributes redirectAttributes) {
        
@@ -67,6 +111,14 @@ public class AuthController {
 
     }
 
+    /**
+     * Invalidates the current session records, logging the current user out.
+     * Redirects to the login page and provides a status message via flash attribute.
+     * @param session the current HttpServletRequest session that needs to be reset
+     * @param redirectAttributes used to attach the status message
+     * @return a redirect to the login page
+     * @author Enterprise App Development Final Project Group
+     */
     @GetMapping("/logout")
     public String logout(HttpServletRequest session, RedirectAttributes redirectAttributes) {
         session.getSession().invalidate();
